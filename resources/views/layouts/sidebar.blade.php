2aca3ddc017d0603e1d1c12b4b8952d1cd4675f4
muzammal-gl <m.muzammal@greelogix.com>
2025-01-17 12:56:06 +0500

    Update sidebar.blade.php

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,32 +1,32 @@
-<div class="container" style="width: 278px; height: 870px; position: relative; top: 0px; background: #7f56d9;">
+<div class="sidebar-crm" style="height: 870px; position: relative; top: 0px; background: #7f56d9; max-width: 250px;">
     <div class="text-white h-100 rounded-3">
-        <ul class="list-unstyled p-4">
+        <ul class="list-unstyled pt-4">
             <li>
                 <a href="{{ route('dashboard') }}" class="d-block px-4 py-2 rounded text-white text-decoration-none hover-bg-dark hover-text-primary d-flex align-items-center">
-                    <i class="fas fa-tachometer-alt me-2"></i> 
-                    Dashboard
+                    <i class="fas fa-home me-2"></i> 
+                    <span class="sidebar-text"> Dashboard</span>
                 </a>
             </li>
             <li>
                 <a href="{{ route('leads.index') }}" class="d-block px-4 py-2 rounded text-white text-decoration-none hover-bg-dark hover-text-primary d-flex align-items-center">
-                    <i class="fas fa-users me-3"></i> 
-                    Proposal
+                    <i class="fas fa-file-signature me-3"></i> 
+                    <span class="sidebar-text"> Proposal</span>
                 </a>
             </li>
             <li>
                 <a href="{{ route('connect') }}" class="d-block px-4 py-2 rounded text-white text-decoration-none hover-bg-dark hover-text-primary d-flex align-items-center">
-                    <i class="fas fa-users me-3"></i> 
-                    Connects
+                    <i class="fas fa-link me-3"></i> 
+                    <span class="sidebar-text"> Connects</span> 
                 </a>
             </li>
             @if(Auth::user()->role == 'admin')
             <li>
                 <a href="{{ route('user.index') }}" class="d-block px-4 py-2 rounded text-white text-decoration-none hover-bg-dark hover-text-primary d-flex align-items-center">
-                    <i class="fas fa-users me-3"></i> 
-                    Users
+                    <i class="fas fa-user-cog me-3"></i> 
+                    <span class="sidebar-text"> Users</span>
                 </a>
             </li>
             @endif
-        </ul>
+        </ul>        
     </div>
 </div>
