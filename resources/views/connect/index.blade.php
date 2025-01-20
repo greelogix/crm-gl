c95c630228e3cd78ca662b6de60818fc53c8ec1f
muzammal-gl <m.muzammal@greelogix.com>
2025-01-20 13:56:29 +0500

    Update index.blade.php

--- resources/views/connect/index.blade.php
+++ resources/views/connect/index.blade.php
@@ -2,18 +2,19 @@
 @extends('layouts.app')
 
 @section('content')
-<div class=" mb-3 d-flex justify-content-between position-relative" style="top: 35px;">
+<div class="mb-5 pb-3 d-flex justify-content-between position-relative" style="top: 35px;">
     <div class="">
         <h5 class="" style="font-size: large;">Available Connects : {{$connects->sum('connects_buy')}}</h5>
     </div>
     <div>
-        <button class="btn btn-purple add-cuctomer create-lead shadow-none"  data-bs-toggle="modal" data-bs-target="#LeadModal" style="font-size: small;">Add Connects</button>
+        <button class="btn btn-purple add-cuctomer create-connect shadow-none"  data-bs-toggle="modal" data-bs-target="#ConnectModal" style="font-size: small;">Add Connects</button>
     </div>
 </div>
-<div class="filters-container mb-3 d-flex gap-3 justify-content-end position-relative" style="top: 35px;">
+{{-- <div class="filters-container mb-3 d-flex gap-3 justify-content-end position-relative" style="top: 35px;">
         <input type="search" id="custom-search" class="form-control w-auto shadow-none" placeholder="Search">
-</div>
-<table id="customerTable" class="table table-bordered align-middle w-100" >
+</div> --}}
+
+<table id="customerTable" class="table align-middle w-100 mt-5">
     <thead class="table-active">
         <tr style="font-size: small; background-color: #f8f9fa;">
             <th></th>
@@ -23,12 +24,14 @@
             <th>Total Price</th>
             <th>Total Connects</th>
             <th>Total Use Connects</th>
+            <th>Remaining Connects</th>
             <th>Action</th>
         </tr>
     </thead>
-    @php $weekNumber = 1; @endphp
+   
     <div class="toggle-row">
         <tbody class="" id="accordionExample">
+            @php $weekNumber = 1; @endphp
             @foreach ($groupedByWeekConnects as $week => $connects)
             <tr style="font-size: small;" class="row-proposal toggle-row table-active" data-toggle="collapseweek-{{ Str::slug($week) }}">
                 <td class="up-arrow"><i class="fa-sharp fa-solid fa-angle-down"></i></td>
@@ -36,15 +39,18 @@
                 <td>{{ Auth::user()->name }}</td>
                 <td>{{ $week }}</td>
                 <td>${{ $connects->sum('price') }}</td>
-                <td>{{ $connects->sum('connects_buy') }}</td>
+                {{-- <td>{{ $connects->sum('connects_buy') }}</td>
                 <td>
                     @php
                         $weekLeads = $groupedByWeekLeads[$week] ?? collect();
                     @endphp
                     {{ $weekLeads->filter(fn($lead) => is_numeric($lead['connects_spent']))->sum('connects_spent') }}
-                </td>
+                </td> --}}
+                <td>{{ $weeklyData[$week]['total_buy'] }}</td>
+                <td>{{ $weeklyData[$week]['total_spent'] }}</td>
+                <td>{{ $weeklyData[$week]['carry_forward'] }}</td>
                 <td class="action-icons">
-                    <button class="btn add-more-btn create-lead" data-bs-toggle="modal" data-bs-target="#LeadModal">
+                    <button class="btn add-more-btn create-connect" data-bs-toggle="modal" data-bs-target="#ConnectModal">
                         Add More
                     </button>
                 </td>
@@ -56,14 +62,14 @@
                         <th>Date</th>
                         <th>Price</th>
                         <th>Connects</th>
-                        <th>Connects Use</th>
+                        {{-- <th>Connects Use</th> --}}
                         <th>Action</th>
                     </tr>
-                   @foreach ($connects->groupBy('date') as $date => $dailyConnects)
+                    {{-- @foreach ($connects->groupBy('date') as $date => $dailyConnects)
                     @php
                         $dailyConnectsByUser = $dailyConnects->groupBy('user_id');
                     @endphp
-
+                
                     @foreach ($dailyConnectsByUser as $userId => $userConnects)
                         <tr style="font-size: small;" class="row-proposal">
                             <td></td>
@@ -77,18 +83,55 @@
                                     $dailyLeads = $weekLeads->where('created_at', '>=', \Carbon\Carbon::parse($date)->startOfDay())
                                                             ->where('created_at', '<=', \Carbon\Carbon::parse($date)->endOfDay())
                                                             ->where('user_id', $userId);
-
+                
                                     $dailyLeadsFiltered = $dailyLeads->filter(fn($lead) => is_numeric($lead['connects_spent']));
                                 @endphp
                                 {{ $dailyLeadsFiltered->sum('connects_spent') }}
                             </td>
+
                             <td class="action-icons">
-                                {{-- You can choose to show any buttons/actions here --}}
+                                <a href="" data-bs-toggle="tooltip" title="Delete Connect">
+                                    <i class="fa-regular fa-trash-can" style="font-size: medium;"></i>
+                                </a>
                             </td>
                         </tr>
-                    @endforeach
+                    @endforeach --}}
+                    @foreach ($connects as $connect)
+                    <tr style="font-size: small; background-color: #f8f9fa;">
+                        <td></td>
+                        <td>{{ $loop->iteration }}</td>
+                        <td>{{ $connect->user->name ?? 'N/A' }}</td>
+                        <td>{{ $connect->date }}</td>
+                        <td>${{ $connect->price }}</td>
+                        <td>{{ $connect->connects_buy }}</td>
+                        {{-- <td>
+                            @php
+                                $dailyLeads = $groupedByWeekLeads[$week] ?? collect();
+                                $dailyLeadsFiltered = $dailyLeads
+                                    ->where('created_at', '>=', \Carbon\Carbon::parse($connect->date)->startOfDay())
+                                    ->where('created_at', '<=', \Carbon\Carbon::parse($connect->date)->endOfDay())
+                                    ->where('user_id', $connect->user_id)
+                                    ->filter(fn($lead) => is_numeric($lead['connects_spent']));
+                            @endphp
+                            {{ $dailyLeadsFiltered->sum('connects_spent') }}
+                        </td> --}}
+                        <td class="action-icons">
+                            <a href="{{ route('connect.destroy', $connect->id) }}" data-bs-toggle="tooltip" title="Delete" onclick="event.preventDefault(); document.getElementById('delete-form-{{ $connect->id }}').submit();">
+                                <i class="fa-regular fa-trash-can" style="font-size: medium;"></i>
+                            </a>
+            
+                            <a href="{{ route('connect.edit', $connect->id) }}" class="cursor-pointer ms-1 edit-connect" data-bs-toggle="modal" data-bs-target="#ConnectModal" data-id="{{ $connect->id }}" data-bs-toggle="tooltip" title="Edit">
+                                <i class="fa fa-pencil" style="font-size: small;"></i>
+                            </a>
+            
+                            <form id="delete-form-{{ $connect->id }}" action="{{ route('connect.destroy', $connect->id) }}" method="POST" style="display: none;">
+                                @csrf
+                               @method('DELETE') 
+                            </form>
+                        </td>
+                        
+                    </tr>
                 @endforeach
-
                 </tbody>
             </tr>
             @endforeach
@@ -98,11 +141,11 @@
 
 
  <!-- Modal -->
- <div class="modal fade" id="LeadModal" tabindex="-1" aria-labelledby="LeadModalLabel" aria-hidden="true">
+ <div class="modal fade" id="ConnectModal" tabindex="-1" aria-labelledby="ConnectModalLabel" aria-hidden="true">
     <div class="modal-dialog  modal-lg">
     <div class="modal-content">
         <div class="modal-header">
-        <h5 class="modal-title" id="LeadModalLabel" style="font-size: medium; color: #7f56d">Add New Connect</h5>
+        <h5 class="modal-title" id="ConnectModalLabel" style="font-size: medium; color: #7f56d">Add New Connect</h5>
         <button type="button" class="btn-close shadow-none" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">
@@ -163,8 +206,9 @@
             paging: true,
             searching: true,
             lengthChange: true,
-            ordering: true
+            ordering: true,
         });
+       
 
  $('#customerTable_length select').removeClass('form-select form-select-sm');
 
@@ -187,6 +231,37 @@
             return;
         }
         this.submit();
+    });
+
+    $('body').on('click', '.create-connect', function(){
+        $('#ConnectModalLabel').text('Add New Connect'); 
+        $('#btn-submit').text('Submit');
+        $('.rate-input').hide();
+        $('#connectform')[0].reset();
+    });
+
+
+    $('body').on('click', '.edit-connect', function(){
+        var connectId = $(this).data('id');
+        $.ajax({
+            url: '/connect/' + connectId + '/edit',
+            type: 'GET',
+            success: function(response) {
+                $('#connectform')[0].reset();
+                $('#name').val(response.connect.name);
+                $('#date').val(response.connect.date);
+                $('#price').val(response.connect.price);
+                $('#connects_buy').val(response.connect.connects_buy);
+                $('#connectform').attr('action', '/connect/' + connectId); 
+                $('#connectform').append('<input type="hidden" name="_method" value="PUT">'); 
+                $('#ConnectModalLabel').text('Update Connect'); 
+                $('#btn-submit').text('Submit');
+                $('#ConnectModal').modal('show');
+            },
+            error: function(xhr, status, error) {
+                toastr.error('Error fetching lead data:', error);
+            }
+        });
     });
 
     $('body').on('input', '.required', function () {
