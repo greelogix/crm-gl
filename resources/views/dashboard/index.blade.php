4a83f968b1f285ac383047426c1938ed32558931
muzammal-gl <m.muzammal@greelogix.com>
2025-02-10 17:45:59 +0500

    Update index.blade.php

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="d-flex mx-auto justify-content-between mt-5">
-
+@if(Auth::user()->role == 'admin')
    <div class="card flex-fill me-3" style="height: 115px;">
        <div class="card-body d-flex justify-content-between align-items-center">
            <span class="text-muted fw-medium">
@@ -11,7 +11,7 @@
            <span class="fw-bold text-dark">{{$usercount}}</span>
        </div>
    </div>
-
+ @endif
    <div class="card flex-fill me-3" style="height: 115px;">
        <div class="card-body d-flex justify-content-between align-items-center">
            <span class="text-muted fw-medium">
